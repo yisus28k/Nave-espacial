be87b40a1b9eab3a753498844444a0a03b0e38d2
Octavio Solorzano Roblero <octaviosolorzano91@gmail.com>
2023-12-07 12:16:58 -0600

    Agregado de imagenes y exe

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ function App() {
 
   return (
     <>
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <main className="container px-4 py-12 mx-auto sm:px-6 lg:px-8">
         <section className="flex flex-col items-center space-y-8">
           <img
             alt="App Logo"
-            className="w-52 h-52 rounded-full mb-4"
+            className="mb-4 rounded-full w-52 h-52"
             height="100"
             src="../public/827343b3-2f11-4316-b658-aac290bfc671.jpg"
             width="100"
@@ -20,7 +20,7 @@ function App() {
           </p>
 
           <button
-            className="inline-flex items-center justify-center text-lg ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary hover:bg-primary/90 h-15 bg-gradient-to-r from-yellow-500 via-red-500 to-purple-500 hover:from-yellow-600 hover:via-red-600 hover:to-purple-600 text-black font-bold py-3 px-6 rounded"
+            className="inline-flex items-center justify-center px-6 py-3 text-lg font-bold text-black transition-colors rounded ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary hover:bg-primary/90 h-15 bg-gradient-to-r from-yellow-500 via-red-500 to-purple-500 hover:from-yellow-600 hover:via-red-600 hover:to-purple-600"
             id="0x8bf5s0tnai"
           >
             <svg
@@ -43,46 +43,46 @@ function App() {
           </button>
         </section>
         <section className="mt-16">
-          <h2 className="text-5xl font-bold mb-8 text-center">Requisitos del sistema</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
+          <h2 className="mb-8 text-5xl font-bold text-center">Requisitos del sistema</h2>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="border rounded-lg shadow-sm bg-card text-card-foreground" data-v0-t="card">
               <div className="flex flex-col space-y-1.5 p-6 bg-gray-100">
                 <h3 className="text-xl font-bold text-center">Mínimos</h3>
               </div>
               <div className="p-4">
-                <ul className="list-disc list-inside space-y-2">
-                  <li>OS: Windows 10, 64-bit</li>
-                  <li>Processor: Intel Core i5-2500K or AMD FX-6300</li>
-                  <li>Memory: 8 GB RAM</li>
-                  <li>Graphics: NVIDIA GeForce GTX 780 or AMD Radeon R9 290</li>
-                  <li>Storage: 70 GB available space</li>
+                <ul className="space-y-2 list-disc list-inside">
+                  <li>OS: Windows 8, 8.1, 32-bit</li>
+                  <li>Processor: Intel Core i3 or AMD a6-4400m</li>
+                  <li>Memory: 4 GB RAM</li>
+                  <li>Graphics: 2.70 ghz</li>
+                  <li>Storage: 10 GB available space</li>
                 </ul>
               </div>
             </div>
-            <div className="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
+            <div className="border rounded-lg shadow-sm bg-card text-card-foreground" data-v0-t="card">
               <div className="flex flex-col space-y-1.5 p-6 bg-gray-100">
                 <h3 className="text-xl font-bold text-center">Recomendados</h3>
               </div>
               <div className="p-4">
-                <ul className="list-disc list-inside space-y-2">
-                  <li>OS: Windows 10, 64-bit</li>
+                <ul className="space-y-2 list-disc list-inside">
+                  <li>OS: Windows 10, 11, 64-bit</li>
                   <li>Processor: Intel Core i7-4770K or AMD Ryzen 5 1500X</li>
                   <li>Memory: 12 GB RAM</li>
                   <li>Graphics: NVIDIA GeForce GTX 1060 or AMD Radeon RX 480</li>
-                  <li>Storage: 70 GB available space</li>
+                  <li>Storage: 20 GB available space</li>
                 </ul>
               </div>
             </div>
           </div>
         </section>
         <section className="mt-16">
-          <h2 className="text-4xl font-bold mb-4 text-center">Capturas de pantalla</h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <h2 className="mb-4 text-4xl font-bold text-center">Capturas de pantalla</h2>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
             <img
               alt="Game screenshot 1"
               className="rounded-lg"
               height="200"
-              src="/placeholder.svg"
+              src="../public/Inicio.jpg"
               width="300"
               style={{ aspectRatio: '300 / 200', objectFit: 'cover' }}
             />
@@ -90,7 +90,7 @@ function App() {
               alt="Game screenshot 2"
               className="rounded-lg"
               height="200"
-              src="/placeholder.svg"
+              src="../public/juego.jpg"
               width="300"
               style={{ aspectRatio: '300 / 200', objectFit: 'cover' }}
             />
@@ -98,7 +98,7 @@ function App() {
               alt="Game screenshot 3"
               className="rounded-lg"
               height="200"
-              src="/placeholder.svg"
+              src="../public/juego1.jpg"
               width="300"
               style={{ aspectRatio: '300 / 200', objectFit: 'cover' }}
             />
@@ -106,7 +106,7 @@ function App() {
               alt="Game screenshot 4"
               className="rounded-lg"
               height="200"
-              src="/placeholder.svg"
+              src="../public/fin.jpg"
               width="300"
               style={{ aspectRatio: '300 / 200', objectFit: 'cover' }}
             />
